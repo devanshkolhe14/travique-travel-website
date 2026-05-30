f3b5966ea9ebc133a1101b3b55488939393ba85f
devanshkolhe14 <devanshkolhe95@gmail.com>
2026-05-30 16:53:00 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLink = document.getElementById("nav-link");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLink.classList.toggle("open");
+
+    const isOpen = navLink.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+})
+
+navLink.addEventListener("click", (e) => {
+    navLink.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+})
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+
+};
+
+ScrollReveal().reveal(".header-image img", {
+    ...scrollRevealOption,
+    origin: "right"
+})
+
+ScrollReveal().reveal(".header-content p", {
+    ...scrollRevealOption,
+    delay: 500
+})
+
+ScrollReveal().reveal(".header-content h1", {
+    ...scrollRevealOption,
+    delay: 1000
+})
+
+ScrollReveal().reveal(".header-btn", {
+    ...scrollRevealOption,
+    delay: 1500
+})
